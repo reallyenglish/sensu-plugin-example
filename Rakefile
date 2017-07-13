79f05bb57c54e803e648bc1427c50dce15e4768a
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-07-13 15:21:01 +0900

    initial PR (#1)
    * bundle gem
    
    * update gitignore
    
    * QA
    
    * QA and add rubocop
    
    * add LICENSE
    
    * make specs pass
    
    * add .github
    
    * add .ackrc
    
    * QA
    
    * add rubocop and test tasks
    
    * restore standard README for sensu-plugin
    
    * s/ROLE/GEM/

---
+++ Rakefile
@@ -0,0 +1,14 @@
+require "bundler/gem_tasks"
+require "rspec/core/rake_task"
+
+RSpec::Core::RakeTask.new(:spec)
+
+task default: :test
+
+desc "Run rubocop"
+task :rubocop do
+  sh "rubocop"
+end
+
+desc "Run tests"
+task test: [:spec, :rubocop]
---
+++ lib/sensu/plugins/example.rb
@@ -0,0 +1,10 @@
+require "sensu/plugins/example/version"
+
+module Sensu
+  module Plugins
+    # This is an example sensu-plugin for demonstration. It is not intened to
+    # be used in production environment.
+    module Example
+    end
+  end
+end
